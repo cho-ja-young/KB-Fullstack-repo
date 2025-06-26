9510798f9ae8ce044a516fc0eca33a6b12c02a9d
cho-ja-young <26006@naver.com>
2025-06-26 13:32:10 +0900

    Modify : vue 프로젝트의 ChangePasswordPage.vue ì수정

--- frontend/src/pages/auth/ChangePasswordPage.vue
+++ frontend/src/pages/auth/ChangePasswordPage.vue
@@ -31,4 +31,9 @@ function changePassword() {
   padding: 20px;
 }
 .change-password input, .change-password button {
-  display: blo
+  display: block;
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 10px;
+}
+</style>
