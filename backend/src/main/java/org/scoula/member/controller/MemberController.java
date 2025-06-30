9e8a09fa46e5b3d9ec345f885373e5b19268237d
chojayoung <26006@naver.com>
2025-06-30 17:02:25 +0900

    Add : backend login logout 코드 추가

--- backend/src/main/java/org/scoula/member/controller/MemberController.java
+++ backend/src/main/java/org/scoula/member/controller/MemberController.java
@@ -2,11 +2,15 @@ package org.scoula.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.scoula.common.util.UploadFiles;
 import org.scoula.member.dto.MemberDTO;
 import org.scoula.member.dto.MemberJoinDTO;
 import org.scoula.member.service.MemberService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 
 @Slf4j
 @RestController
@@ -25,5 +29,18 @@ public class MemberController {
     @PostMapping("")
     public ResponseEntity<MemberDTO> join(MemberJoinDTO member) {
         return ResponseEntity.ok(service.join(member));
+    }
+
+    @GetMapping("/{username}/avatar")
+    public void getAvatar(@PathVariable String username, HttpServletResponse response) {
+        String avatarPath = "/Users/jordy/Documents/upload/avatar/" + username + ".png";
+        File file = new File(avatarPath);
+
+        if(!file.exists()) {
+            // 아바타가 없는 경우 기본 이미지 사용
+            file = new File("/Users/jordy/Documents/upload/avatar/unknown.png");
+        }
+
+        UploadFiles.downloadImage(response, file);
     }
 }
